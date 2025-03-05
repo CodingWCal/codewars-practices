eb3cdef9dcde632475439c4bb75f5ea3b3a1e17c
Calvin V. <calvintrinhvan@gmail.com>
2025-03-04 20:11:06 -0500

    Add files via upload

---
+++ codewars-banjo-check-8kyu/are-you-playing-banjo-check.js
@@ -0,0 +1,27 @@
+//PROMPT:
+// Create a function which answers the question "Are you playing banjo?".
+//If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+//The function takes a name as its only argument, and returns one of the following strings:
+
+//name + " plays banjo" 
+//name + " does not play banjo"
+//Names given are always valid strings
+
+//SOLUTION
+function areYouPlayingBanjo(name) {
+    // Implement me
+    if(name.charAt(0) === 'R' ||
+    name.charAt(0) === 'r') {
+    return name + " plays banjo" ;
+    } else{
+      return name + " does not play banjo";
+    }
+  }
+  
+  //PERSONAL PSEUDOCODE
+  // Check first letter of the name.
+  
+  //Determine if it is either uppercase "R" or lowercase "r".
+  
+  //Return string based on the check.
